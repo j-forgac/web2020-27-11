17ffdf17100e8a0b52047c3f1d11cfcc60761875
Jan Forgac <jan.forgac@educanet.cz>
2020-11-27 12:44:38 +0100

    snadbox fixed and working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/index.js', './styles.scss'],
+    entry: ['./src/index.js', './src/styles.scss'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
