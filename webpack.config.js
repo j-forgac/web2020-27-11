eb5b2e258e14f2f57040e6bd651e6af6615aa37e
Jan Forgac <jan.forgac@educanet.cz>
2020-11-27 11:58:34 +0100

    sandbox webpack + bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['./src/index.js', './styles.scss'],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            // Prefer `dart-sass`
+                            implementation: require('sass'),
+                        },
+                    },
+                ],
+            },
+        ],
+    }
+};
